1f14b1fa33134d69c37d2cfe0afc91906ad8dc3f
Leandro Souza <souza.leandro78@gmail.com>
2025-01-22 23:26:24 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,21 @@
+print('\n### Sabor Express ###')
+print('''
+      1. Cadastrar restaurante
+      2. Listar restaurantes
+      3. Ativar restaurante
+      4. Sair
+      ''')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida) #Esta seria uma alternativa à linha anterior
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 3:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 4: #na solução original do curso os instrutores usaram else, mas assim o programa executaria o código qualquer que fosse a entrada, desde que diferente das anteriores. Prefiro retornar um erro
+    print('Encerrando programa')
+
+# Uma opção para a mensagem de erro seria colocar tudo dentro de uma estrutura de repetição, mas ainda não sei como fazer isso.
